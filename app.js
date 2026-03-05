df2e21887261f586cab43daf3acfcd7d5cac52f4
myztinen <myztinen@gmail.com>
2026-03-05 18:01:51 +0200

    11.10 -11.12 Added healthcheck url and trigger

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.use(express.static('dist'))
