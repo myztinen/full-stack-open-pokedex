580b6a2b9363aab52192ecfb7b3261b2cd937003
myztinen <myztinen@gmail.com>
2026-03-08 18:52:37 +0200

    11.18 changed deployment action also message. Tests should work now

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -20,7 +20,7 @@ describe('<App />', () => {
     await act(async () => {
       render(<Router><App /></Router>)
     })
-    expect(axiosMock.get).toHaveBeenCalledTimes(2)
+    expect(axiosMock.get).toHaveBeenCalledTimes(1)
     expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
   })
 
